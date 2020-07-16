2810c3464a6057b0c964d2c181099a6b202a83fc
Serene <lingserene12@gmail.com>
2020-07-16 16:04:30 +0800

    Extract CSS  &  minify HTML/CSS/JS

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,25 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // 3. Injects style into DOM
+          "css-loader", // 2. Turns css into commonjs
+          "sass-loader", // 1. Turns sass into css
+        ],
+      },
+    ],
   },
 });
